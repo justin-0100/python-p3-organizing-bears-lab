63f58f15a15253fba958355723fad817613dabb5
Justin <justinagui988@gmail.com>
2025-07-03 10:45:51 +0300

    Create bears table, insert records, and implement SQL queries

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 10, 'M', 'brown', 'calm', 1),
+('Rowdy', 7, 'M', 'black', 'aggressive', 1),
+('Tabitha', 4, 'F', 'red', 'friendly', 1),
+('Sergeant Brown', 8, 'M', 'dark brown', 'strict', 1),
+('Melissa', 6, 'F', 'blonde', 'playful', 1),
+('Grinch', 9, 'M', 'green', 'grumpy', 0),
+('Wendy', 5, 'F', 'white', 'cheerful', 1),
+(NULL, 3, 'M', 'grey', 'mysterious', 1);
